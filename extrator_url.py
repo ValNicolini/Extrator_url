68aacf639766e63db95f3aa8802429aa27de6d2e
ValNicolini <val_nicolini13@hotmail.com>
2023-11-24 16:05:29 -0300

    Empresa

---
+++ extrator_url.py
@@ -0,0 +1,20 @@
+class ExtratorURL:
+    def __init__(self, url):
+     self.url = self.sanitiza_url(url)
+     self.valida_url()
+     def sanitiza_url(self, url):
+         return url.strip()
+     def valida_url(self):
+         if url == '':
+             raise ValueError('A url está vazia')
+
+
+
+
+
+
+
+
+extrator_url = ExtratorURL('bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real&quantidade=100')
+valor_quantidade = extrator_url.get_valor_parametro('quantidade')
+print(valor_quantidade)
--- main.py
+++ main.py
@@ -1,28 +1,26 @@
-# url = "https;//bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real&quantidade=100"
-#
-#
-# #separa a base e parâmetros
-# indice_interrogacao = url.find("?")
+url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real&quantidade=100"
+#url = "  "
+if url.strip() == "":
+  raise ValueError("A url está vazia")
+#separa a base e parâmetros
+indice_interrogacao = url.find("?")
 # print(indice_interrogacao)
-# url_base = url[:indice_interrogacao]
-# print(url_base)
-# url_parametros = url[indice_interrogacao+1:]
-#
-# print(url_parametros)
-#
-# #Busca o valor de um parâmetro
-# parametro_busca = 'quantidade'
-# indice_parametro = url_parametros.find(parametro_busca)
-# indice_valor = indice_parametro + len(parametro_busca)+1
-# indice_e_comercial = url_parametros.find("&", indice_valor)
-# if indice_e_comercial == -1:
-#   valor = url_parametros[indice_valor:]
-#
-# else:
-#     valor = url_parametros[indice_valor:indice_e_comercial]
-# print(valor)
+url_base = url[:indice_interrogacao]
+print(url_base)
+url_parametros = url[indice_interrogacao+1:]
+
+print(url_parametros)
+
+#Busca o valor de um parâmetro
+parametro_busca = 'moedaDestino'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca)+1
+indice_e_comercial = url_parametros.find("&", indice_valor)
+if indice_e_comercial == -1:
+  valor = url_parametros[indice_valor:]
+
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
 
 
-url = "https://www.alura.com.br/curso?curso=python"
-indice_curso = url.find("curso")
-print(indice_curso)
