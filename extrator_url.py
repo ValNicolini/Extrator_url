df50eab9f636232e3647dba23da012aa91a702b9
ValNicolini <val_nicolini13@hotmail.com>
2023-11-27 15:52:05 -0300

    Update extrator_url.py

--- extrator_url.py
+++ extrator_url.py
@@ -1,12 +1,12 @@
 class ExtratorURL:
-    def __init__(self, url):
-     self.url = self.sanitiza_url(url)
-     self.valida_url()
-     def sanitiza_url(self, url):
-         return url.strip()
-     def valida_url(self):
-         if url == '':
-             raise ValueError('A url está vazia')
+        def __init__(self, url):
+         self.url = self.sanitiza_url(url)
+         self.valida_url()
+        def sanitiza_url(self, url):
+             return url.strip()
+        def valida_url(self):
+            if self.url == '':
+              raise ValueError('A url está vazia')
 
 
 
@@ -15,6 +15,7 @@ class ExtratorURL:
 
 
 
-extrator_url = ExtratorURL('bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real&quantidade=100')
-valor_quantidade = extrator_url.get_valor_parametro('quantidade')
-print(valor_quantidade)
+#extrator_url = ExtratorURL('bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real&quantidade=100')
+extrator_url = ExtratorURL(' ')
+#valor_quantidade = extrator_url.get_valor_parametro('quantidade')
+#print(valor_quantidade)
