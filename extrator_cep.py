e06e6490de2b5d9f7cadd1a917480b43199a1b5f
ValNicolini <val_nicolini13@hotmail.com>
2023-12-21 16:30:47 -0300

    Update extrator_cep.py

--- extrator_cep.py
+++ extrator_cep.py
@@ -1,13 +1,54 @@
-endereco = 'Rua das Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440-120'
+endereco = """<ADVOGADO>FLAVIO NEVES COSTA</ADVOGADO>
+    <OABNUMERO></OABNUMERO>
+    <OABESTADO></OABESTADO>
+    <DIARIO>DJSC</DIARIO>
+    <EDICAODIARIO>0</EDICAODIARIO>
+    <PAGINAINICIAL>0</PAGINAINICIAL>
+    <DATAPUBLICACAO>03/10/2023</DATAPUBLICACAO>
+    <NPROCESSO>5005638-58.2021.8.24.0030</NPROCESSO>
+    <CIDADEPUBLICACAO>FLORIANÓPOLIS</CIDADEPUBLICACAO>
+    <ORGAO>TJ Santa Catarina</ORGAO>
+    <VARA>1ª CÂMARA DE DIREITO COMERCIAL FLORIANÓPOLIS</VARA>
+    <DESPACHOPUBLICACAO>DIÁRIO DE JUSTIÇA ELETRÔNICO NACIONAL (DJEN)</DESPACHOPUBLICACAO>
+    <PROCESSOPUBLICACAO>Processo 5005638-58.2021.8.24.0030 Data de disponibilização: 02/10/2023 Órgão: 1ª Câmara de Direito Comercial Tipo de comunicação: Intimação Meio: Diário Eletrônico de Justiça Nacional Inteiro teor: https://eproc2g.tjsc.jus.br/eproc/externo_controlador.php?acao consulta_publica_pauta&amp;idSessao 321669399873012659479833888535&amp;hash 8d2b2539b16d0707abe65c22b98c0cfe555b5b80d319aeb8789eac1610eecca2 Parte(s): SEGREDO DE JUSTIçA SEGREDO DE JUSTIçA Advogado(s): FLAVIO NEVES COSTA - OAB SC-SC056707 1ª Câmara de Direito Comercial Pauta de Julgamentos Torno público que, de acordo com o artigo 934 do Código de Processo Civil c/c art. 142-L do Regimento Interno do Tribunal de Justiça de Santa Catarina, na Sessão Totalmente Virtual do dia 19 de outubro de 2023, quinta-feira, às 14h00min, serão julgados os seguintes processos: Apelação Nº 5005638-58.2021.8.24.0030/SC (Pauta: 519)RELATOR: Desembargador JOSÉ MAURÍCIO LISBOA Publique-se e Registre-se.Florianópolis, 29 de setembro de 2023. Desembargador GUILHERME NUNES BORN Presidente id: 59126715</PROCESSOPUBLICACAO>
+  </PUBLICACAO>
+  <PUBLICACAO>
+    <ADVOGADO>FLAVIO NEVES COSTA</ADVOGADO>
+    <OABNUMERO></OABNUMERO>
+    <OABESTADO></OABESTADO>
+    <DIARIO>DJSC</DIARIO>
+    <EDICAODIARIO>0</EDICAODIARIO>
+    <PAGINAINICIAL>0</PAGINAINICIAL>
+    <DATAPUBLICACAO>03/10/2023</DATAPUBLICACAO>
+    <NPROCESSO>5028044-95.2023.8.24.0000</NPROCESSO>
+    <CIDADEPUBLICACAO>FLORIANÓPOLIS</CIDADEPUBLICACAO>
+    <ORGAO>TJ Santa Catarina</ORGAO>
+    <VARA>1ª CÂMARA DE DIREITO COMERCIAL FLORIANÓPOLIS</VARA>
+    <DESPACHOPUBLICACAO>DIÁRIO DE JUSTIÇA ELETRÔNICO NACIONAL (DJEN)</DESPACHOPUBLICACAO>
+    <PROCESSOPUBLICACAO>Processo 5028044-95.2023.8.24.0000 Data de disponibilização: 02/10/2023 Órgão: 1ª Câmara de Direito Comercial Tipo de comunicação: Intimação Meio: Diário Eletrônico de Justiça Nacional Inteiro teor: https://eproc2g.tjsc.jus.br/eproc/externo_controlador.php?acao consulta_publica_pauta&amp;idSessao 321669399873012659479833888535&amp;hash 8d2b2539b16d0707abe65c22b98c0cfe555b5b80d319aeb8789eac1610eecca2 Parte(s): MARIA BERNADETE KLUCK AYMORE CREDITO, FINANCIAMENTO E INVESTIMENTO S.A. Advogado(s): GREGORY PIMENTEL - OAB SC-SC067480A FLAVIO NEVES COSTA - OAB SC-SC056707 1ª Câmara de Direito Comercial Pauta de Julgamentos Torno público que, de acordo com o artigo 934 do Código de Processo Civil c/c art. 142-L do Regimento Interno do Tribunal de Justiça de Santa Catarina, na Sessão Totalmente Virtual do dia 19 de outubro de 2023, quinta-feira, às 14h00min, serão julgados os seguintes processos: Agravo de Instrumento Nº 5028044-95.2023.8.24.0000/SC (Pauta: 409) RELATOR: Desembargador LUIZ ZANELATO AGRAVANTE: MARIA BERNADETE KLUCK ADVOGADO(A): GREGORY PIMENTEL (OAB SC067480A) AGRAVADO: AYMORE CREDITO, FINANCIAMENTO E INVESTIMENTO S.A. ADVOGADO(A): FLAVIO NEVES COSTA (OAB SC056707) Publique-se e Registre-se.Florianópolis, 29 de setembro de 2023. Desembargador GUILHERME NUNES BORN Presidente id: 59126456</PROCESSOPUBLICACAO>
+  </PUBLICACAO>
+  <PUBLICACAO>
+    <ADVOGADO>FLAVIO NEVES COSTA</ADVOGADO>
+    <OABNUMERO></OABNUMERO>
+    <OABESTADO></OABESTADO>
+    <DIARIO>DJSC</DIARIO>
+    <EDICAODIARIO>0</EDICAODIARIO>
+    <PAGINAINICIAL>0</PAGINAINICIAL>
+    <DATAPUBLICACAO>03/10/2023</DATAPUBLICACAO>
+    <NPROCESSO>5014982-79.2020.8.24.0036</NPROCESSO>
+    <CIDADEPUBLICACAO>FLORIANÓPOLIS</CIDADEPUBLICACAO>
+    <ORGAO>TJ Santa Catarina</ORGAO>
+    <VARA>1ª CÂMARA DE DIREITO COMERCIAL FLORIANÓPOLIS</VARA>
+    <DESPACHOPUBLICACAO>DIÁRIO DE JUSTIÇA ELETRÔNICO NACIONAL (DJEN)</DESPACHOPUBLICACAO>
+    <PROCESSOPUBLICACAO>Processo 5014982-79.2020.8.24.0036 Data de disponibilização: 02/10/2023 Órgão: 1ª Câmara de Direito Comercial Tipo de comunicação: Intimação Meio: Diário Eletrônico de Justiça Nacional Inteiro teor: https://eproc2g.tjsc.jus.br/eproc/externo_controlador.php?acao consulta_publica_pauta&amp;idSessao 321669399873012659479833888535&amp;hash 8d2b2539b16d0707abe65c22b98c0cfe555b5b80d319aeb8789eac1610eecca2 Parte(s): BANCO VOLKSWAGEN S.A. JOSE GREGORIO RIBEIRO FILHO Advogado(s): FLAVIO NEVES COSTA - OAB SP-SP153447 1ª Câmara de Direito Comercial Pauta de Julgamentos Torno público que, de acordo com o artigo 934 do Código de Processo Civil c/c art. 142-L do Regimento Interno do Tribunal de Justiça de Santa Catarina, na Sessão Totalmente Virtual do dia 19 de outubro de 2023, quinta-feira, às 14h00min, serão julgados os seguintes processos: Apelação Nº 5014982-79.2020.8.24.0036/SC (Pauta: 357) RELATOR: Desembargador LUIZ ZANELATO APELANTE: BANCO VOLKSWAGEN S.A. (AUTOR) ADVOGADO(A): FLAVIO NEVES COSTA (OAB SP153447) APELADO: JOSE GREGORIO RIBEIRO FILHO (RÉU) Publique-se e Registre-se.Florianópolis, 29 de setembro de 2023. Desembargador GUILHERME NUNES BORN """
 
 import re  #regular expression -- RegEx
 
 #5 dígitos + hífen(opcional) + 3 dígitos
 
-padrao = re.compile('[0-9]{5}[-]{0,1}[0-9]{3}')
+padrao = re.compile('[0-9]{7}[-][0-9]{2}[.][0-9]{4}[.][1-9]{1}[.][0-9]{2}[.][0-9]{4}')
 busca = padrao.search(endereco)  #Match
-if busca:
-    cep = busca.group()
-    print(cep)
-else:
-    print('CEP não encontrado!')
+
+cep = busca.group()
+for n in cep:
+    print(n, end='')
